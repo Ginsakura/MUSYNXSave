e3c75a5c2c807c1c3f02aa80811c4c1aa952240c
ginsakura <ai.0121.000.256@gmail.com>
2023-03-10 13:05:27 +0800

    提供编译脚本

---
+++ compile.ps1
@@ -0,0 +1,6 @@
+﻿pyinstaller -F -i ./musync/Musync.ico ./MusyncSavDecodeGUI.py -w
+Rename-Item -Path ".\dist\MusyncSavDecodeGUI.exe" -NewName "MusyncSavDecodeGUI_NoConsole.exe"
+pyinstaller -F -i ./musync/Musync.ico ./MusyncSavDecodeGUI.py
+Rename-Item -Path ".\dist\MusyncSavDecodeGUI.exe" -NewName "MusyncSavDecodeGUI_WithConsole.exe"
+Start-Sleep -Seconds 5
+Copy-Item -Path ".\dist\MusyncSavDecodeGUI_WithConsole.exe" -Destination "./MusyncSavDecodeGUI.exe"

3fff8ce621b86a82b600fad50f3340b07caf0337
ginsakura <ai.0121.000.256@gmail.com>
2023-03-10 23:19:44 +0800

    update

--- compile.ps1
+++ compile.ps1
@@ -1,8 +1,8 @@
 ﻿Remove-Item -Path ".\dist\MusyncSavDecodeGUI_WithConsole.exe"
 Remove-Item -Path ".\dist\MusyncSavDecodeGUI_NoConsole.exe"
-pyinstaller -F -i ./musync/Musync.ico ./MusyncSavDecodeGUI.py -w
+pyinstaller -F -i ./musync_data/Musync.ico ./MusyncSavDecodeGUI.py -w
 Rename-Item -Path ".\dist\MusyncSavDecodeGUI.exe" -NewName "MusyncSavDecodeGUI_NoConsole.exe"
-pyinstaller -F -i ./musync/Musync.ico ./MusyncSavDecodeGUI.py
+pyinstaller -F -i ./musync_data/Musync.ico ./MusyncSavDecodeGUI.py
 Rename-Item -Path ".\dist\MusyncSavDecodeGUI.exe" -NewName "MusyncSavDecodeGUI_WithConsole.exe"
 Start-Sleep -Seconds 5
 Copy-Item -Path ".\dist\MusyncSavDecodeGUI_WithConsole.exe" -Destination "./MusyncSavDecodeGUI.exe"

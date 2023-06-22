24b00f4605a1f724ff5057845d86b0779311e996
unknown <ai.0121.000.256@gmail.com>
2023-06-22 22:05:48 +0800

    1.2.2rc2  修复编译环境缺少 uiautomation 库的bug  Release

--- MusyncSavDecodeGUI.py
+++ MusyncSavDecodeGUI.py
@@ -18,7 +18,7 @@ import Functions
 #import win32gui_struct
 #import win32gui
 #from threading import Thread
-version = '1.2.2rc1'
+version = '1.2.2rc2'
 
 class MusyncSavDecodeGUI(object):
 	"""docstring for MusyncSavDecodeGUI"""

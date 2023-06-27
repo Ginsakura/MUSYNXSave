6e84a98edb2ca7db124a88db4611bd32c9fd33a4
Ginsakura <ai.0121.000.256@gmail.com>
2023-06-28 00:17:00 +0800

    1.2.3rc4  修复Total统计中CyanExtra统计错误的bug 程序优化(小) 错误修复(中)  Release

--- MusyncSavDecodeGUI.py
+++ MusyncSavDecodeGUI.py
@@ -19,7 +19,7 @@ import Functions
 #import win32gui_struct
 #import win32gui
 #from threading import Thread
-version = '1.2.3rc3'
+version = '1.2.3rc4'
 
 class MusyncSavDecodeGUI(object):
 	"""docstring for MusyncSavDecodeGUI"""

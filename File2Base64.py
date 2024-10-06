dd9f2f08c7f341b02311f109bfb091d6aaab2a31
Ginsakura <ai.0121.000.256@gmail.com>
2024-10-06 17:46:16 +0800

    1.2.8rc1  优化内部存储文件的编解码方式  Release

--- File2Base64.py
+++ File2Base64.py
@@ -5,30 +5,48 @@ from base64 import b64encode,b64decode
 
 def EncodeIcon():
 	with open("./musync_data/MUSYNC.ico", 'rb+') as icon:
-		with open("./musync_data/icon.b64", 'wb+') as iconEncode:
-			iconEncode.write(b64encode(icon.read()))
+		with open("./musync_data/icon.b64", 'w') as iconEncode:
+			data = b64encode(icon.read()).decode("ascii")
+			iconEncode.write("\"")
+			for i in range(len(data)//100+1):
+				iconEncode.write(f"{data[i*100:i*100+100]}\\\n")
+			iconEncode.write("\"")
 
 def EncodeTTF():
 	with open("./musync_data/LXGW.ttf", 'rb+') as ttf:
-		with open("./musync_data/ttf.b64", 'wb+') as ttfEncode:
-			ttfEncode.write(b64encode(ttf.read()))
+		with open("./musync_data/ttf.b64", 'w') as ttfEncode:
+			data = b64encode(ttf.read()).decode("ascii")
+			ttfEncode.write("\"")
+			for i in range(len(data)//100+1):
+				ttfEncode.write(f"{data[i*100:i*100+100]}\\\n")
+			ttfEncode.write("\"")
 
 def EncodeJson():
 	with open("./musync_data/songname.json", 'rb+') as snj:
-		with open("./musync_data/songname.b64", 'wb+') as snjEncode:
-			b64e = b64encode(snj.read())
-			snjEncode.write(b64e)
-			pyperclip.copy(b64e.decode('utf8'))
+		with open("./musync_data/songname.b64", 'w') as snjEncode:
+			data = b64encode(snj.read()).decode("ascii")
+			snjEncode.write("\"")
+			for i in range(len(data)//100+1):
+				snjEncode.write(f"{data[i*100:i*100+100]}\\\n")
+			snjEncode.write("\"")
 
 def EncodeDLL():
 	with open('./musync_data/Assembly-CSharp.dll','rb') as hdf:
-		with open('./musync_data/Assembly-CSharp.b64','wb') as hdfE:
-			hdfE.write(b64encode(hdf.read()))
+		with open('./musync_data/Assembly-CSharp.b64','w') as hdfE:
+			data = b64encode(hdf.read()).decode("ascii")
+			hdfE.write("\"")
+			for i in range(len(data)//100+1):
+				hdfE.write(f"{data[i*100:i*100+100]}\\\n")
+			hdfE.write("\"")
 
 def EncodeLicense():
 	with open('./LICENSE', 'rb') as license:
-		with open('./musync_data/LICENSE.b64','wb') as licenseE:
-			licenseE.write(b64encode(license.read()))
+		with open('./musync_data/LICENSE.b64','w') as licenseE:
+			data = b64encode(license.read()).decode("ascii")
+			licenseE.write("\"")
+			for i in range(len(data)//100+1):
+				licenseE.write(f"{data[i*100:i*100+100]}\\\n")
+			licenseE.write("\"")
 			
 if __name__ == '__main__':
 	if not os.path.isfile("./musync_data/ttf.b64"):

dee4a802dd8fce8f1f956731e90971496b0805ea
Ginsakura <lvjie_073@outlook.com>
2025-12-20 01:01:35 +0800

    fix some problem in script

--- Version.py
+++ Version.py
@@ -1,5 +1,5 @@
 version:str = '2.1.0'						# Release版本号
 preVersion:str = "2.1.0pre20"				# PreRelease版本号
 # 提交前手动控制
-# Change 0→1 for release
-isPreRelease:bool = False if 0 else True
+# Change 1 to 0 for release
+isPreRelease:bool = not 1

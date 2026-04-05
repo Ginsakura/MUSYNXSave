d9f80f34b8ed528d02821c77ef2b24198863f96b
Ginsakura <lvjie_073@outlook.com>
2026-04-05 23:18:18 +0800

    完成pre号自增功能；修复main_window.py命名错误的问题；修复github action配置错误的问题

--- musync_save/version.py
+++ musync_save/version.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
 version:str = '3.0.0'						# Release版本号
-pre_version:str = "3.0.0pre17"				# PreRelease版本号
-# 提交前手动控制
-# Change 1 to 0 for release
-is_pre_release:bool = not 0
+pre_version:str = '3.0.0pre22'				# PreRelease版本号
+is_pre_release:bool = True

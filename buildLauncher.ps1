6182ce1e4d8a3719038a4b4c02621a7c6efd5bd4
Ginsakura <lvjie_073@outlook.com>
2026-04-06 00:18:47 +0800

    YAML & PS1 bug fix

--- buildLauncher.ps1
+++ buildLauncher.ps1
@@ -4,25 +4,15 @@ param (
     [switch]$noarchive
 )
 
-# 定义函数
-# 构建图标资源
+# ==========================================
+# 函数定义区
+# ==========================================
+
+<#
+.SYNOPSIS
+构建C/C++图标资源
+#>
 function BuildIcon{
-    <#
-    .SYNOPSIS
-    构建C/C++图标资源
-    .DESCRIPTION
-    构建C/C++图标资源
-    .PARAMETER None
-    无参数
-    .EXAMPLE
-    BuildIcon
-    .NOTES
-    提供其他重要信息，如注意事项、限制条件、已知问题等。
-    .INPUTS
-    描述函数可以接收的输入类型，通常用于支持管道输入的函数。
-    .OUTPUTS
-    描述函数的输出类型和内容。
-    #>
     $resPath = "ico.res";   # C++图标res资源
     $rcPath = "ico.rc";     # C++图标rc资源
     Set-Location -Path "musync_data/";
@@ -35,41 +25,19 @@ function BuildIcon{
     Set-Location -Path ..;
 }
 
-# 目录检查
+<#
+.SYNOPSIS
+检查目录是否存在，如果不存在则创建该目录。
+.PARAMETER Dir
+指定要检查的目录路径。
+#>
 function CheckDir {
-    <#
-    .SYNOPSIS
-    检查目录是否存在，如果不存在则创建该目录。
-
-    .DESCRIPTION
-    此函数检查指定的目录是否存在。如果目录不存在，将创建该目录，并返回成功消息。
-    如果目录已存在，则返回相应的提示消息。
-
-    .PARAMETER Dir
-    指定要检查的目录路径。
-
-    .EXAMPLE
-    CheckDir -Dir "C:\tempdir"
-    检查目录 "C:\tempdir" 是否存在，如果不存在则创建该目录。
-
-    .EXAMPLE
-    "C:\tempdir" | CheckDir
-    通过管道输入目录路径，检查目录是否存在，如果不存在则创建该目录。
-
-    .NOTES
-    此函数适用于需要确保目录存在的场景。如果目录路径无效（例如包含非法字符），可能会导致错误。
-
-    .INPUTS
-    此函数接受字符串类型的目录路径作为输入。支持通过管道输入。
-
-    .OUTPUTS
-    此函数不返回任何输出，但会通过 Write-Host 输出状态消息。
-    #>
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
         [string]$Dir
     )
+
 
     process {
         if (-not (Test-Path -Path $Dir)) {
@@ -81,28 +49,17 @@ function CheckDir {
     }
 }
 
-# 复制资源文件
+<#
+.SYNOPSIS
+Copies specified resources from a source directory to a target directory.
+.PARAMETER SourceDirectory
+The directory containing the source files to be copied.
+.PARAMETER TargetDirectory
+The directory where the files will be copied to.
+.PARAMETER ResourceFiles
+An array of file names to be copied.
+#>
 function Copy-Resources {
-    <#
-    .SYNOPSIS
-    Copies specified resources from a source directory to a target directory.
-
-    .DESCRIPTION
-    This function takes a list of resource files, copies them from the source directory to the target directory,
-    and provides feedback on the success or failure of each copy operation.
-
-    .PARAMETER SourceDirectory
-    The directory containing the source files to be copied.
-
-    .PARAMETER TargetDirectory
-    The directory where the files will be copied to.
-
-    .PARAMETER ResourceFiles
-    An array of file names to be copied.
-
-    .EXAMPLE
-    Copy-Resources -SourceDirectory "musync_data" -TargetDirectory "MusyncSaveDecode/musync_data" -ResourceFiles @("Resources.bin", "songname.json", "songname.ver", "Musync.ico")
-    #>
     param (
         [string]$SourceDirectory,
         [string]$TargetDirectory,
@@ -123,24 +80,15 @@ function Copy-Resources {
     }
 }
 
-# 定义打包函数
-function Compress-Archive {
-    <#
-    .SYNOPSIS
-    创建ZIP文件，将指定的文件和文件夹打包。
-
-    .DESCRIPTION
-    此函数根据提供的文件列表和目标路径，使用Compress-Archive命令创建ZIP文件。
-
-    .PARAMETER SourceItems
-    要打包的文件和文件夹路径数组。
-
-    .PARAMETER DestinationZip
-    输出的ZIP文件路径。
-
-    .EXAMPLE
-    Compress-Archive -SourceItems @("logs", "musync_data", "MusyncSaveDecodeCLI.exe") -DestinationZip "MusyncSaveDecode_WithConsole_1.0.0_AllInOne.zip"
-    #>
+<#
+.SYNOPSIS
+创建ZIP文件，将指定的文件和文件夹打包。
+.PARAMETER SourceItems
+要打包的文件和文件夹路径数组。
+.PARAMETER DestinationZip
+输出的ZIP文件路径。
+#>
+function Create-Compress-Archive {
     param (
         [Parameter(Mandatory = $true)]
         [string[]]$SourceItems,
@@ -231,12 +179,12 @@ Copy-Resources -SourceDirectory "../musync_data" -TargetDirectory "MusyncSaveDec
 if ($noarchive){
     Write-Host "Skip Archive Zip File";
 } else {
-    Compress-Archive -SourceItems $archive_AC -DestinationZip $destinationZip_AC;
-    Compress-Archive -SourceItems $archive_ANC -DestinationZip $destinationZip_ANC;
+    Create-Compress-Archive -SourceItems $archive_AC -DestinationZip $destinationZip_AC;
+    Create-Compress-Archive -SourceItems $archive_ANC -DestinationZip $destinationZip_ANC;
     Set-Location -Path "MusyncSaveDecodeCLI";
-    Compress-Archive -SourceItems $archive_C -DestinationZip $destinationZip_C;
+    Create-Compress-Archive -SourceItems $archive_C -DestinationZip $destinationZip_C;
     Set-Location -Path "../MusyncSaveDecodeNoCLI";
-    Compress-Archive -SourceItems $archive_NC -DestinationZip $destinationZip_NC;
+    Create-Compress-Archive -SourceItems $archive_NC -DestinationZip $destinationZip_NC;
     Set-Location -Path "..";
 }
 Set-Location -Path "..";
